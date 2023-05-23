b28af203db5102603746ea42deb3bbe20c7dec58
Polaris_Light <995905922@qq.com>
2023-05-23 19:34:08 +0800

    upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# Hitokoto
+
+Use `!!hitokoto` to get a hitokoto!
+使用命令 `!!hitokoto` 来获取一句一言！
---
+++ hitokoto/__init__.py
@@ -0,0 +1,49 @@
+import json
+import os
+from typing import List, Callable, Any
+import urllib.request
+
+
+from mcdreforged.api.all import *
+
+
+class Config(Serializable):
+      c: List[str] = [
+            'a'
+      ]
+      min_length: int = 0
+      max_length: int = 30
+
+Prefix = '!!hitokoto'
+config: Config
+ConfigFilePath = os.path.join('config', 'kitokoto.json')
+
+
+def get_url():
+    url = 'https://v1.hitokoto.cn/?min_length={}&max_length={}'.format(config.min_length, config.max_length)
+    for type in config.c:
+        url = url + '&c={}'.format(type)
+    return url
+
+def get_hitokoto(url): 
+    json_string = urllib.request.urlopen(url, timeout=30).read().decode('UTF-8', 'strict')
+    map = json.loads(json_string)
+    who = '未知' if map['from_who'] is None else map['from_who']
+    hitokoto = '{}——{} {}'.format(map['hitokoto'], who, map['from'])
+    return hitokoto
+
+
+def display_hitokoto(reply: Callable[[str], Any]):
+    hitokoto = get_hitokoto(get_url())
+    reply(hitokoto)
+
+
+def on_player_joined(server: ServerInterface, player, info):
+	display_hitokoto(lambda msg: server.tell(player, msg))
+
+
+def on_load(server: PluginServerInterface, old):
+    global config
+    config = server.load_config_simple(file_name=ConfigFilePath, in_data_folder=False, target_class=Config)
+    server.register_help_message(Prefix, '获取一句一言！')
+    server.register_command(Literal(Prefix).runs(lambda src: display_hitokoto(src.reply)))
